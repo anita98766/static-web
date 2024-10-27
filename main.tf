0949f6f2fe8baacb40db35d2c63373a8e6b1fc8a
anita98766 <141495916+anita98766@users.noreply.github.com>
2024-10-27 23:23:28 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -3,7 +3,7 @@ provider "aws" {
 }
 
 resource "aws_s3_bucket" "website_bucket" {
-  bucket = "myfirstbucket5091230011456u"
+  bucket = "myfirstbucket5088a"
 }
 
 resource "aws_s3_object" "website_bucket" {
@@ -92,7 +92,7 @@ resource "aws_cloudfront_distribution" "cdn_static_site" {
 }
 
 resource "aws_cloudfront_origin_access_control" "default" {
-  name                              = "example8"
+  name                              = "example9"
   description                       = "description of OAC"
   origin_access_control_origin_type = "s3"
   signing_behavior                  = "always"
